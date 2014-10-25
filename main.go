54503ec8aa64759e32e9fafe8c5b3d0e0d3d9399
yosssi <yoshida.keiji.84@gmail.com>
2014-10-25 23:58:51 +0900

    Update main.go

--- main.go
+++ main.go
@@ -25,9 +25,9 @@ func main() {
 		log.Panic(err)
 	}
 
-	var conf Config
+	var cnf Config
 
-	if err := yaml.Unmarshal(b, &conf); err != nil {
+	if err := yaml.Unmarshal(b, &cnf); err != nil {
 		log.Panic(err)
 	}
 
@@ -37,14 +37,14 @@ func main() {
 
 	defer rpio.Close()
 
-	chLED, chLEDDone := ledOn()
+	chLED, chLEDDone := ledOn(cnf)
 
 	defer func() {
 		close(chLED)
 		<-chLEDDone
 	}()
 
-	pin := rpio.Pin(conf.MotionPinNo)
+	pin := rpio.Pin(cnf.MotionPinNo)
 	pin.Input()
 
 	log.Println("Ready")
@@ -59,13 +59,22 @@ func main() {
 	}
 }
 
-func ledOn() (chan<- struct{}, <-chan struct{}) {
+func ledOn(cnf Config) (chan<- struct{}, <-chan struct{}) {
 	chLED := make(chan struct{}, bfSizeChLED)
 	chLEDDone := make(chan struct{})
 
 	go func() {
+		pin := rpio.Pin(cnf.LEDPinNo)
+		pin.Output()
+
 		for _ = range chLED {
 			log.Println("LED on")
+
+			pin.High()
+
+			time.Sleep(1 * time.Second)
+
+			pin.Low()
 		}
 
 		chLEDDone <- struct{}{}
